c4e5793930eda213a7b33d43e973a6f8c780304c
Arikrishnan01 <121212706+Arikrishnan01@users.noreply.github.com>
2023-05-28 22:33:19 -0700

    Update Create.js
    updated

--- src/Create.js
+++ src/Create.js
@@ -97,7 +97,7 @@ export default function Create (){
                     },
                 })
                   .then((data) => data.json())
-                  .then(() => navigate("/create"))
+                  .then(() => navigate("/"))
               }}
             >
             Add User</Button>
